d2ea3d7aaa6f305246dffab47011e7fa8bafc2c3
selfsame <joseph.parker.1@gmail.com>
2014-07-08 18:06:26 -0400

    still trying to build correctly

--- project.clj
+++ project.clj
@@ -1,26 +1,28 @@
 (defproject org.clojars.selfsame/plomb "0.1.0-SNAPSHOT"
   :description "sugar macros for core.async chans between om components"
-  :url "https://github.com/selfsame/plomb"
+  :url "http://github.com/selfsame/plomb"
   :license {:name "The MIT License (MIT)"
             :url "https://github.com/selfsame/plomb/blob/master/LICENSE"}
 
   :source-paths ["src"]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha" :scope "provided"]
                  [om "0.6.4"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
 
   :cljsbuild {:builds
-              [{:id "main"
-                :source-paths ["src"]
-                :compiler {
-                           :output-to "core.js"
-                           :output-dir "out"
-                           :optimizations :none}}
+              [{:source-paths ["src"]
+                :id "release"
+                :compiler
+                {:output-to "core.js"
+                 :output-dir "out"
+                 :optimizations :advanced
+                 :output-wrapper false
+                 :pretty-print false}}
                {:id "simple"
                 :source-paths ["src" "examples/simple/src"]
                 :compiler {
